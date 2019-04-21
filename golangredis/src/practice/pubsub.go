e2f94ee527e86118bd8392e558a43a7a6c4f0864
unknown <fuli314@outlook.com>
2019-04-21 22:14:06 +0800

    fix:

--- golangredis/src/practice/pubsub.go
+++ golangredis/src/practice/pubsub.go
@@ -4,20 +4,47 @@ package main
 import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"time"
 )
 
-func main(){
-	conn, err := redis.Dial("tcp","192.168.75.134:6379")
+func Subs(str string) {  //消费者
+	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
 	if err != nil {
-		fmt.Println("err:",err)
+		fmt.Println("connect redis error :", err)
 		return
 	}
-	pubSub := redis.PubSubConn{
-		Conn: conn,
-	}
-	err = pubSub.Ping("ping")
-	if err != nil {
-		fmt.Println(err)
+	defer conn.Close()
+	psc := redis.PubSubConn{conn}
+	psc.Subscribe(str) //订阅channel1频道
+	for {
+		switch v := psc.Receive().(type) {
+		case redis.Message:
+			fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
+		case redis.Subscription:
+			fmt.Printf("hello %s: %s %d\n", v.Channel, v.Kind, v.Count)
+		case error:
+			fmt.Println(v)
+			return
+		}
 	}
 }
 
+func Push(message string)  { //生产者
+	conn, _ := redis.Dial("tcp", "127.0.0.1:6379")
+	str := make([]string,0)
+	str = append(str,"channel.md", "channel.text", "channel.log")
+	_,err1 := conn.Do("PUBLISH", str, message)
+	if err1 != nil {
+		fmt.Println("pub err: ", err1)
+		return
+	}
+
+}
+
+func main()  {
+	//需要先启动消费者
+	go Subs("channel1")
+	//go Subs("channel2")
+	go Push("hello world h")
+	time.Sleep(time.Second*3)
+}
